acdab0cb45ff61d1755580f18367544db31b1252
simti <simti@rocketmail.com>
2020-09-13 20:41:36 +0430

    add hdri in progress

--- app/js/main.js
+++ app/js/main.js
@@ -1,198 +1,198 @@
-//libraries
-import * as THREE from 'three';
-import {
-	GLTFLoader
-} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import {
-	OrbitControls
-} from "three/examples/jsm/controls/OrbitControls";
-import {
-	RectAreaLightUniformsLib
-} from 'three/examples/jsm/lights/RectAreaLightUniformsLib.js';
-import {
-	RectAreaLightHelper
-} from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
+import * as THREE from 'three/build/three.module.js';
+
+			// import Stats from 'three/examples/jsm/libs/stats.module.js';
+
+			// import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js';
+			import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+      import { HDRCubeTextureLoader } from 'three/examples/jsm/loaders/HDRCubeTextureLoader.js';
+      import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+
+			var params = {
+				envMap: 'HDR',
+				roughness: 0.0,
+				metalness: 0.0,
+				exposure: 1.0,
+				debug: false
+			};
+
+			var container;
+			var camera, scene, renderer, controls;
+			var torusMesh, planeMesh;
+			var hdrCubeRenderTarget;
+			var hdrCubeMap;
+
+			init();
+			animate();
+
+			function getEnvScene() {
+
+				var envScene = new THREE.Scene();
+
+				var geometry = new THREE.BoxBufferGeometry();
+				geometry.deleteAttribute( 'uv' );
+				var roomMaterial = new THREE.MeshStandardMaterial( { metalness: 0, side: THREE.BackSide } );
+				var room = new THREE.Mesh( geometry, roomMaterial );
+				room.scale.setScalar( 10 );
+				envScene.add( room );
+
+				var mainLight = new THREE.PointLight( 0xffffff, 50, 0, 2 );
+				envScene.add( mainLight );
+
+				var lightMaterial = new THREE.MeshLambertMaterial( { color: 0x000000, emissive: 0xffffff, emissiveIntensity: 10 } );
+
+				var light1 = new THREE.Mesh( geometry, lightMaterial );
+				light1.material.color.setHex( 0xff0000 );
+				light1.position.set( - 5, 2, 0 );
+				light1.scale.set( 0.1, 1, 1 );
+				envScene.add( light1 );
+
+				var light2 = new THREE.Mesh( geometry, lightMaterial.clone() );
+				light2.material.color.setHex( 0x00ff00 );
+				light2.position.set( 0, 5, 0 );
+				light2.scale.set( 1, 0.1, 1 );
+				envScene.add( light2 );
+
+				var light3 = new THREE.Mesh( geometry, lightMaterial.clone() );
+				light3.material.color.setHex( 0x0000ff );
+				light3.position.set( 2, 1, 5 );
+				light3.scale.set( 1.5, 2, 0.1 );
+				envScene.add( light3 );
+
+				return envScene;
+
+			}
+
+			function init() {
+
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
+				camera.position.set( 0, 0, 120 );
+
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0x000000 );
+
+				renderer = new THREE.WebGLRenderer();
+				renderer.physicallyCorrectLights = true;
+				renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
+				//
+
+				var geometry = new THREE.TorusKnotBufferGeometry( 18, 8, 150, 20 );
+				//var geometry = new THREE.SphereBufferGeometry( 26, 64, 32 );
+				var material = new THREE.MeshStandardMaterial( {
+					color: 0xffffff,
+					metalness: params.metalness,
+					roughness: params.roughness
+				} );
+
+				torusMesh = new THREE.Mesh( geometry, material );
+				scene.add( torusMesh );
 
 
-var camera, controls, scene, renderer, rectLight1, rectLight2, rectLight3;
-let simti = []
+				var geometry = new THREE.PlaneBufferGeometry( 200, 200 );
+				var material = new THREE.MeshBasicMaterial();
 
-init();
-//render(); // remove when using next line for animation loop (requestAnimationFrame)
-animate();
+				planeMesh = new THREE.Mesh( geometry, material );
+				planeMesh.position.y = - 50;
+				planeMesh.rotation.x = - Math.PI * 0.5;
+				scene.add( planeMesh );
 
-function init() {
+				THREE.DefaultLoadingManager.onLoad = function ( ) {
 
-	scene = new THREE.Scene();
-	// scene.background = new THREE.Color(0xcccccc);
-	// scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
-	// scene.add(new THREE.GridHelper(10, 10));
+					pmremGenerator.dispose();
 
-	const bg_loader = new THREE.CubeTextureLoader();
-	const texture = bg_loader
-		.load([
-			'dist/env/px.jpg',
-			'dist/env/nx.jpg',
-			'dist/env/py.jpg',
-			'dist/env/ny.jpg',
-			'dist/env/pz.jpg',
-			'dist/env/nz.jpg'
-		]);
-	scene.background = texture;
+				};
 
+				var hdrUrls = [ 'px.hdr', 'nx.hdr', 'py.hdr', 'ny.hdr', 'pz.hdr', 'nz.hdr' ];
+				hdrCubeMap = new HDRCubeTextureLoader()
+					.setPath( 'dist/env/' )
+					.setDataType( THREE.UnsignedByteType )
+					.load( hdrUrls, function () {
 
+						hdrCubeRenderTarget = pmremGenerator.fromCubemap( hdrCubeMap );
 
-	renderer = new THREE.WebGLRenderer({
-		antialias: true
-	});
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
+						hdrCubeMap.magFilter = THREE.LinearFilter;
+						hdrCubeMap.needsUpdate = true;
+
+          } );
+          
+
+        // hdrCubeMap = new RGBELoader()
+        //   .setDataType( THREE.UnsignedByteType )
+        //   .setPath( 'textures/equirectangular/' )
 
 
-	camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 2000);
-	camera.position.set(400, 200, 0);
 
-	// controls
-	controls = new OrbitControls(camera, renderer.domElement);
-	controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-	controls.dampingFactor = 0.05;
-	controls.screenSpacePanning = false;
-	controls.minDistance = 100;
-	controls.maxDistance = 1000;
-	controls.maxPolarAngle = Math.PI * 4;
+				var pmremGenerator = new THREE.PMREMGenerator( renderer );
+				pmremGenerator.compileCubemapShader();
+
+				var envScene = getEnvScene();
+				// generatedCubeRenderTarget = pmremGenerator.fromScene( envScene, 0.04 );
+
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+
+				//renderer.toneMapping = ReinhardToneMapping;
+				renderer.outputEncoding = THREE.sRGBEncoding;
 
 
-	//add car 
-	let car;
-	var loader = new GLTFLoader();
+				controls = new OrbitControls( camera, renderer.domElement );
+				controls.minDistance = 50;
+				controls.maxDistance = 300;
 
-	loader.load(
-		// resource URL
-		'dist/new.gltf',
-		// called when the resource is loaded
-		function (gltf) {
-			console.log(gltf.scene.children[0].children[0].children[0].children[0].children[0].children[0])
+				window.addEventListener( 'resize', onWindowResize, false );
+			}
 
-			gltf.scene.traverse(function (object) {
+			function onWindowResize() {
 
-				if (object.isMesh) {
-					if (object.name == "gum001_carpaint_0") {
-						object.material.metalness = 0.2061918556690216;
-						object.material.roughness = 0.2278855562210083;
-					}
-					simti.push(object)
+				var width = window.innerWidth;
+				var height = window.innerHeight;
+
+				camera.aspect = width / height;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( width, height );
+
+			}
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+				render();
+
+			}
+
+			function render() {
+
+				torusMesh.material.roughness = params.roughness;
+				torusMesh.material.metalness = params.metalness;
+
+				var renderTarget, cubeMap;
+        renderTarget = hdrCubeRenderTarget;
+        cubeMap = hdrCubeMap;
+
+
+				var newEnvMap = renderTarget ? renderTarget.texture : null;
+
+				if ( newEnvMap && newEnvMap !== torusMesh.material.envMap ) {
+
+					torusMesh.material.envMap = newEnvMap;
+					torusMesh.material.needsUpdate = true;
+
+					planeMesh.material.map = newEnvMap;
+					planeMesh.material.needsUpdate = true;
+
 				}
 
-			});
+				torusMesh.rotation.y += 0.005;
+				planeMesh.visible = params.debug;
 
-			console.log(simti)
+				scene.background = cubeMap;
+				renderer.toneMappingExposure = params.exposure;
 
-			scene.add(gltf.scene);
+				renderer.render( scene, camera );
 
-			gltf.animations; // Array<THREE.AnimationClip>
-			gltf.scene; // THREE.Group
-			gltf.scenes; // Array<THREE.Group>
-			gltf.cameras; // Array<THREE.Camera>
-			gltf.asset; // Object
-
-		},
-		// called while loading is progressing
-		function (xhr) {
-
-			console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-		},
-		// called when loading has errors
-		function (error) {
-
-			console.log('An error happened');
-
-		}
-	);
-
-
-	// lights
-
-	// var light = new THREE.DirectionalLight( 0xffffff );
-	// light.position.set( 1, 1, 1 );
-	// scene.add( light );
-
-	// RectAreaLightUniformsLib.init();
-
-	rectLight1 = new THREE.RectAreaLight(0xffffff, 5, 300, 300);
-	rectLight1.position.set(0, 900, 0);
-	rectLight1.rotateX(THREE.Math.degToRad(-90));
-	scene.add(rectLight1);
-
-
-	rectLight2 = new THREE.RectAreaLight(0xffffff, 5, 100, 300);
-	rectLight2.position.set(0, 300, 300);
-	rectLight2.rotateX(THREE.Math.degToRad(-45));
-	scene.add(rectLight2);
-
-	rectLight3 = new THREE.RectAreaLight(0xffffff, 5, 100, 300);
-	rectLight3.position.set(0, 300, -300);
-	rectLight3.rotateX(THREE.Math.degToRad(-135));
-	scene.add(rectLight3);
-
-
-	var helper1 = new RectAreaLightHelper(rectLight1);
-	rectLight1.add(helper1);
-	var helper2 = new RectAreaLightHelper(rectLight2);
-	rectLight2.add(helper2);
-	var helper3 = new RectAreaLightHelper(rectLight3);
-	rectLight3.add(helper3);
-
-	// var light = new THREE.DirectionalLight( 0x002288 );
-	// light.position.set( - 1, - 1, - 1 );
-	// scene.add( light );
-
-	// var light = new THREE.AmbientLight( 0x222222 );
-	// scene.add( light );
-
-	//
-
-	window.addEventListener('resize', onWindowResize, false);
-
-}
-
-function onWindowResize() {
-
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-
-	renderer.setSize(window.innerWidth, window.innerHeight);
-
-}
-
-function animate() {
-
-	requestAnimationFrame(animate);
-
-	// var t = ( Date.now() / 2000 );
-
-	// 	// move light in circle around center
-	// 	// change light height with sine curve
-
-	// 	var r = 15.0;
-
-	// 	var lx = r * Math.cos( t );
-	// 	var lz = r * Math.sin( t );
-
-	// 	var ly = 5.0 + 5.0 * Math.sin( t / 3.0 );
-
-	// 	rectLight.position.set( lx, ly, lz );
-	// 	rectLight.lookAt( origin );
-
-	controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
-
-	render();
-
-}
-
-function render() {
-
-	renderer.render(scene, camera);
-
-}
+			}
